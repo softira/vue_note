3d6ba556facbdde4b64f8eab76851d499c0c60fe
tanshuai <1160390705@qq.com>
2022-06-13 17:00:15 +0800

    vue核心和生命周期

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <school></school>
+    <student></student>
+  </div>
+</template>
+
+<script>
+  import school from './school.vue'
+  import student from './student.vue'
+
+  export default {
+    name:'App',
+    components:{
+      school,
+      student
+    }
+  }
+</script>
+
+<style>
+
+</style>
